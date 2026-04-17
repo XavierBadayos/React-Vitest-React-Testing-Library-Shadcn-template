f849d7ac0b015141957422ed346a1dfdea478834
XavierBadayos <xavierbadayos@gmail.com>
2026-04-18 03:44:01 +0930

    configure react testing library

--- vite.config.ts
+++ vite.config.ts
@@ -13,5 +13,9 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  test: {},
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: 'src/lib/test/setup.ts'
+  },
 })
